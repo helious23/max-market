976d584d276fb3b35611b7be5216254cd0bf5925
Max16 <max16@naver.com>
2022-03-31 15:49:39 +0900

    Favorite Products part Two

--- pages/api/products/[id]/index.ts
+++ pages/api/products/[id]/index.ts
@@ -7,7 +7,10 @@ const handler = async (
   req: NextApiRequest,
   res: NextApiResponse<ResponseType>
 ) => {
-  const { id } = req.query;
+  const {
+    query: { id },
+    session: { user },
+  } = req;
   const product = await client.product.findUnique({
     where: {
       id: +id.toString(),
@@ -37,10 +40,22 @@ const handler = async (
       },
     },
   });
+  const isLiked = Boolean(
+    await client.fav.findFirst({
+      where: {
+        productId: product?.id,
+        userId: user?.id,
+      },
+      select: {
+        id: true,
+      },
+    })
+  );
 
   res.json({
     ok: true,
     product,
+    isLiked,
     relatedProducts,
   });
 };
--- pages/products/[id].tsx
+++ pages/products/[id].tsx
@@ -6,6 +6,8 @@ import useSWR from "swr";
 import { Product, User } from "@prisma/client";
 import Link from "next/link";
 import Head from "next/head";
+import useMutation from "../../libs/client/useMutation";
+import { cls } from "@libs/client/utils";
 
 interface ProductWithUser extends Product {
   user: User;
@@ -15,6 +17,7 @@ interface IItemDetailResponse {
   ok: boolean;
   product: ProductWithUser;
   relatedProducts: Product[];
+  isLiked: boolean;
 }
 
 const ItemDetail: NextPage = () => {
@@ -22,6 +25,10 @@ const ItemDetail: NextPage = () => {
   const { data } = useSWR<IItemDetailResponse>(
     router.query.id ? `/api/products/${router.query.id}` : null
   );
+  const [toggleFav] = useMutation(`/api/products/${router.query.id}/fav`);
+  const onFavClick = () => {
+    toggleFav({});
+  };
 
   return (
     <Layout title={data?.product.name || "상세 정보"} canGoBack>
@@ -56,22 +63,45 @@ const ItemDetail: NextPage = () => {
             </p>
             <div className="flex items-center justify-between space-x-2">
               <Button text="판매자와 대화하기" large />
-              <button className="flex items-center justify-center p-3 text-gray-400 rounded-md hover:bg-gray-100 hover:text-gray-500 focus:outline-none">
-                <svg
-                  className="w-6 h-6 "
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  aria-hidden="true"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
-                  />
-                </svg>
+              <button
+                onClick={onFavClick}
+                className={cls(
+                  "flex items-center justify-center rounded-md p-3 hover:bg-gray-100 focus:outline-none",
+                  data?.isLiked
+                    ? "text-red-500 hover:text-red-600"
+                    : "text-gray-400  hover:text-gray-500 "
+                )}
+              >
+                {data?.isLiked ? (
+                  <svg
+                    className="w-6 h-6"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                ) : (
+                  <svg
+                    className="w-6 h-6 "
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+                    />
+                  </svg>
+                )}
               </button>
             </div>
           </div>
