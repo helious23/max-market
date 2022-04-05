8f8b9f804abffa80f61c6987f6556c267e7e4679
Max16 <max16@naver.com>
2022-04-05 17:47:48 +0900

    Responses and Redirections on middleware

---
+++ pages/_middleware.ts
@@ -0,0 +1,13 @@
+import type { NextRequest, NextFetchEvent } from "next/server";
+import { NextResponse } from "next/server";
+
+export function middleware(req: NextRequest, event: NextFetchEvent) {
+  if (req.ua?.isBot) {
+    return new Response("봇은 안돼요. 사람만 되요.", { status: 403 });
+  }
+  if (!req.url.includes("/api")) {
+    if (!req.url.includes("/enter") && !req.cookies.maxSession) {
+      return NextResponse.redirect(`${req.nextUrl.origin}/enter`);
+    }
+  }
+}
