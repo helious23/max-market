38a8617100b9e65df6a3b80b2a54412a18e8b46a
Max16 <max16@naver.com>
2022-04-07 16:00:30 +0900

    ISR

--- pages/community/index.tsx
+++ pages/community/index.tsx
@@ -5,6 +5,8 @@ import Layout from "@components/layout";
 import useSWR from "swr";
 import { Post, User } from "@prisma/client";
 import useCoords from "../../libs/client/useCoords";
+import { GetStaticProps } from "next";
+import client from "@libs/server/client";
 
 interface PostWithAnswerWondering extends Post {
   _count: { answers: number; wondering: number };
@@ -12,78 +14,74 @@ interface PostWithAnswerWondering extends Post {
 }
 
 interface IPostsResponse {
-  ok: boolean;
+  // ok: boolean;
   posts: PostWithAnswerWondering[];
 }
 
-const Community: NextPage = () => {
-  const { latitude, longitude } = useCoords();
-  const { data } = useSWR<IPostsResponse>(
-    latitude && longitude
-      ? `/api/posts?latitude=${latitude}&longitude=${longitude}`
-      : null
-  );
+const Community: NextPage<IPostsResponse> = ({ posts }) => {
+  // const { latitude, longitude } = useCoords();
+  // const { data } = useSWR<IPostsResponse>(
+  //   latitude && longitude
+  //     ? `/api/posts?latitude=${latitude}&longitude=${longitude}`
+  //     : null
+  // );
 
   return (
     <Layout seoTitle="동네생활" title="동네생활" hasTabBar>
       <div className="px-4 pb-5 space-y-8">
-        {data ? (
-          data?.posts?.map((post) => (
-            <Link key={post.id} href={`/community/${post.id}`}>
-              <a className="flex flex-col items-start">
-                <span className="flex items-center rounded-full bg-gray-100 px-2.5 py-0.5 text-xs font-medium text-gray-800">
-                  동네질문
+        {posts?.map((post) => (
+          <Link key={post.id} href={`/community/${post.id}`}>
+            <a className="flex flex-col items-start">
+              <span className="flex items-center rounded-full bg-gray-100 px-2.5 py-0.5 text-xs font-medium text-gray-800">
+                동네질문
+              </span>
+              <div className="mt-2 text-gray-700">
+                <span className="font-medium text-orange-500">Q.</span>
+                {post.question}
+              </div>
+              <div className="flex items-center justify-between w-full mt-5 text-xs font-medium text-gray-500">
+                <span>{post.user.name}</span>
+                <span>{post.createdAt}</span>
+              </div>
+              <div className="mt-3 flex w-full space-x-5 border-t border-b-[2px] py-2.5 text-gray-700">
+                <span className="flex items-center space-x-2 text-sm">
+                  <svg
+                    className="w-4 h-4"
+                    fill="none"
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+                    ></path>
+                  </svg>
+                  <span>궁금해요 {post?._count?.wondering}</span>
                 </span>
-                <div className="mt-2 text-gray-700">
-                  <span className="font-medium text-orange-500">Q.</span>
-                  {post.question}
-                </div>
-                <div className="flex items-center justify-between w-full mt-5 text-xs font-medium text-gray-500">
-                  <span>{post.user.name}</span>
-                  <span>{post.createdAt}</span>
-                </div>
-                <div className="mt-3 flex w-full space-x-5 border-t border-b-[2px] py-2.5 text-gray-700">
-                  <span className="flex items-center space-x-2 text-sm">
-                    <svg
-                      className="w-4 h-4"
-                      fill="none"
-                      stroke="currentColor"
-                      viewBox="0 0 24 24"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
-                      ></path>
-                    </svg>
-                    <span>궁금해요 {post._count.wondering}</span>
-                  </span>
-                  <span className="flex items-center space-x-2 text-sm">
-                    <svg
-                      className="w-4 h-4"
-                      fill="none"
-                      stroke="currentColor"
-                      viewBox="0 0 24 24"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
-                      ></path>
-                    </svg>
-                    <span>답변 {post._count.answers}</span>
-                  </span>
-                </div>
-              </a>
-            </Link>
-          ))
-        ) : (
-          <div className="text-center">로딩 중...</div>
-        )}
+                <span className="flex items-center space-x-2 text-sm">
+                  <svg
+                    className="w-4 h-4"
+                    fill="none"
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
+                    ></path>
+                  </svg>
+                  <span>답변 {post?._count?.answers}</span>
+                </span>
+              </div>
+            </a>
+          </Link>
+        ))}
         <FloatingButton href="/community/write">
           <svg
             className="w-6 h-6"
@@ -103,6 +101,23 @@ const Community: NextPage = () => {
       </div>
     </Layout>
   );
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+  console.log(
+    "=====================Building community STATICALLY====================="
+  );
+  const posts = await client.post.findMany({
+    include: {
+      user: true,
+    },
+  });
+  return {
+    props: {
+      posts: JSON.parse(JSON.stringify(posts)),
+    },
+    revalidate: 20,
+  };
 };
 
 export default Community;
---
+++ posts/04-fourth-post.md
@@ -0,0 +1,15 @@
+---
+title: Fourth post
+date: 2022. 04. 09.
+category: post
+---
+
+
+# Hello World!
+
+To do list
+
+- [x] SSR
+- [ ] NextJS
+
+
